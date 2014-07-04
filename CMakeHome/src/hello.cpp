ae6fb9c0099ebfcfc7898efb6f7dcfc5678fe41a
Daniel <burney@hotmail.de>
2014-07-04 16:25:40 +0200

    Set up CMake

---
+++ CMakeHome/src/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "hello.h"
+
+
+void hello() {
+    std::string message = get_message();
+    std::cout << message << std::endl;
+}
+
+std::string get_message() {
+    return "Hellx, World!";
+}
+
